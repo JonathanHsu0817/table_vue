36ce218bcb8bdc755271542380a64615363fbf73
Jonathan0817 <00339028@email.ntou.edu.tw>
2023-01-15 09:16:00 +0800

    增加樣式&modal功能

--- js/index.js
+++ js/index.js
@@ -1,55 +1,84 @@
-import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.45/vue.esm-browser.prod.min.js";
+import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.45/vue.esm-browser.prod.min.js"
 
-
-let tooltipList = "";
+let toolTips ='';
+let tableModal =``;
+let delModal = ``;
 
 const app = {
     data(){
         return{
+            baseUrl:`http://localhost:3000`,
             title:`個人資料表`,
-            lists:[
-                {
-                    name:"邱小甘",
-                    nameInEnglish:"Peter",
-                    gender:"男",
-                    cellphone: "0988797548",
-                    email: "cccexample@gmail.com"
-                },
-                {
-                    name:"蔡凡昕",
-                    nameInEnglish:"Allen",
-                    gender:"男",
-                    cellphone: "0985342835",
-                    email: "wwexample@gmail.com"
-                },
-                {
-                    name:"趙雪瑜",
-                    nameInEnglish:"Sharon",
-                    gender:"女",
-                    cellphone: "0968524127",
-                    email: "68example@gmail.com"
-                },
-                {
-                    name:"賴佳蓉",
-                    nameInEnglish:"Yoki",
-                    gender:"女",
-                    cellphone: "0985557425",
-                    email: "862xample@gmail.com"
-                }
-            ]
+            tempData:{},
+            isNew:false,
+            lists:[]
         }
     },
     methods:{
-        hoverTooltips(){
-            
-            
+        toPhoneFormat(){
+            this.lists.forEach(list=>{
+                let phone =``;
+                // return `${num.substr(0,4)}-${num.substr(4,3)}-${num.substr(7,3)}`
+            })
+        },
+        getProfilesData(){
+            axios.get(`${this.baseUrl}/profile`)
+            .then(res=>{
+                console.log(res);
+                this.lists = res.data;
+            })
+            .catch(err=>{
+                alert(err.response.data.message);
+            })
+        },
+        updateProfilesData(){
+            axios.put(`${this.baseUrl}/profile/${this.tempData.id}`)
+            .then(res=>{
+                alert(res.data.message);
+                tableModal.hide();
+                this.getProfilesData();
+            })
+            .catch(err=>{
+                alert(err.response.data.message);
+            })
+        },
+        delProfilesData() {
+            axios.delete(`${this.baseUrl}/profile/${this.tempData.id}`)
+            .then((response) => {
+              alert(response.data.message);
+              delModal.hide();
+              this.getProfilesData();
+            }).catch((err) => {
+              alert(err.response.data.message);
+            })
+        },
+        openModal(isNew,item){
+            if(isNew === "new"){
+                this.tempData = {}
+                this.isNew = true;
+                tableModal.show();
+            }else if(isNew === "edit"){
+                this.tempData ={...item};
+                this.isNew = false;
+                tableModal.show();
+            }else if(isNew === "delete"){
+                this.tempData ={...item};
+                delModal.show();
+            }
         }
     },
-    mounted(){
+    computed:{
         
-        document.querySelectorAll("#tool").forEach(t=>{
-            new bootstrap.Tooltip(t);
-        })
+    },
+    mounted(){
+        this.getProfilesData();
+        setTimeout(()=>{
+            toolTips = document.querySelectorAll("#tool").forEach(t=>{
+                new bootstrap.Tooltip(t);
+            })
+        },500)
+        tableModal = new bootstrap.Modal(document.querySelector("#tableModal"));
+        delModal = new bootstrap.Modal(document.querySelector("#delModal"));
     }
 }
 
